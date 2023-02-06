e9b06f704a59868aa3d0aa9d20bcea98c84ae466
Patrick Gomes <patrick_epcar17@outlook.com>
2023-02-06 19:16:33 -0300

    upd: requisição com projection.

--- src/utils/app.ts
+++ src/utils/app.ts
@@ -1,15 +1,16 @@
 import express, { Request, Response } from 'express';
-import { Document, FindCursor } from 'mongodb';
+import { Decimal128, Document, FindCursor } from 'mongodb';
 import connect  from './db';
 const app = express();
 
 app.use(express.json());
 
+
 const db = connect.connectDb();
 
 app.get('/teste', async (req: Request, res: Response) => {
   try {
-    const datas: FindCursor<Document> = db.collection('users').find();
+    const datas: FindCursor<Document> = db.collection('users').find({ key: 22 }).project({ name: 1 });
     const mappedCursor: FindCursor<Document> = datas.map(doc => doc);
     const keyCounts: any[] = await mappedCursor.toArray();
     return res.status(200).json(keyCounts);
